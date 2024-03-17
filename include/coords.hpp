ab0d6604b86fdea6d2cb9af4a05e940ddd18fa6f
Squizel <iustin224@gmail.com>
2024-03-17 21:28:11 +0200

    Added various operators for coords

--- include/coords.hpp
+++ include/coords.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "functional"
+#include <algorithm>
+#include <ostream>
 
 struct Coords {
 	int x = 0;
@@ -12,6 +14,30 @@ struct Coords {
 
 	[[nodiscard]] Coords operator+(const Coords &other) const {
 		return {x + other.x, y + other.y};
+	}
+	[[nodiscard]] Coords operator-(const Coords &other) const {
+		return {x - other.x, y - other.y};
+	}
+
+	static Coords max(const Coords &left, const Coords &right) {
+		return {
+			std::max(left.x, right.x),
+			std::max(left.y, right.y),
+		};
+	}
+	static Coords min(const Coords &left, const Coords &right) {
+		return {
+			std::min(left.x, right.x),
+			std::min(left.y, right.y),
+		};
+	}
+
+	friend std::ostream &operator<<(std::ostream &os, const Coords &rhs) {
+
+		os << "x: " << rhs.x
+		   << " y: " << rhs.y;
+
+		return os;
 	}
 };
 
